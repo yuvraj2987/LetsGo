d49f8a81bab9e7cfee27bacde52f69bc8433c082
Amit Kulkarni <yuvraj2987@gmail.com>
2020-01-22 18:34:56 -0800

    Remove array code from hello pkg

--- hello/main.go
+++ hello/main.go
@@ -1,20 +1,8 @@
 package main
 
 import (
-	// "flag"
 	"fmt"
 )
-
-/*
-func main() {
-	ptrO := flag.Bool("o", false, "Boolean option")
-	ptrI := flag.Int("i", 0, "Interger option")
-	flag.Parse()
-	fmt.Println("Boolean Value", *ptrO)
-	fmt.Println("Integer Value", *ptrI)
-
-}
-*/
 
 func main() {
 	fmt.Println("Hi	")
