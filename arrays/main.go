7c90abf93f6428ac793435502f6f998191ace049
Amit Kulkarni <yuvraj2987@gmail.com>
2020-02-14 06:01:42 -0800

    Test 3d array utiliztation

--- arrays/main.go
+++ arrays/main.go
@@ -20,4 +20,20 @@ func main() {
 	c := [2]int{1, 3}
 
 	fmt.Println("a == b", a == b, "\ta == c", a == c, "\t	b == c", b == c)
+	oneD := [4]int{1, 2, 3, 4}
+	fmt.Println("oneD array len ", len(oneD))
+
+	threeD := [3][3][3]int{{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}},
+		{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}},
+		{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}}
+
+	for _, v1 := range threeD {
+		for _, v2 := range v1 {
+			for l3, v3 := range v2 {
+				fmt.Printf("{%v}=>%d\t", l3, v3)
+			}
+			fmt.Println()
+		}
+		fmt.Println()
+	}
 }
