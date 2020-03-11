a971af44665692714bba4affea52de12ad8c324a
Amit Kulkarni <yuvraj2987@gmail.com>
2020-03-11 07:01:39 -0700

    Fix array tests

--- internal/ccr/ch1/arrays_test.go
+++ internal/ccr/ch1/arrays_test.go
@@ -1,20 +1,19 @@
-package main
+package chapter1
 
 import "testing"
 
 func TestIsUnique(t *testing.T) {
-	tests := [] struct {
-		   	str string
+	tests := []struct {
+		str string
 
-			ans bool
-		   }{
-		   	{"abcd", true},
-			{"aabc", false},
-			{"", true}
-		   }
+		ans bool
+	}{
+		{"abcd", true},
+		{"aabc", false},
+		{"", true}}
 
 	for _, test := range tests {
-		if isUnique(test.str) != test.str {
+		if isUnique(test.str) != test.ans {
 			t.Errorf("IsUnique Test failed for %s", test.str)
 		}
 	}
