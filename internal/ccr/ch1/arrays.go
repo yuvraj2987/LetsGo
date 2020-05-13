56f5d8bbb92f74f78d7857f77dfa7290a73eaa5b
Amit Kulkarni <yuvraj2987@gmail.com>
2020-05-13 08:09:07 -0700

    WIP: Add checkPermutations method

--- internal/ccr/ch1/arrays.go
+++ internal/ccr/ch1/arrays.go
@@ -19,6 +19,39 @@ func isUnique(str string) bool {
 	return true
 }
 
+func checkPermutation(s1 string, s2 string) {
+	if len(s1) != len(s2) {
+		return false
+	}
+	// s1 and s2 have same length
+	aMap := make(map[rune]int)
+	for _, ch1 := range s1 {
+		_, exists := aMap[ch1]
+		if exists {
+			aMap[ch1]++
+		} else {
+			aMap[ch1] = 1
+		}
+	}
+
+	for _, ch2 := range s2 {
+		_, exists := aMap[ch2]
+		if exists {
+			aMap[ch2]--
+		} else {
+			// ch1 in s1 is not in s2
+			return false
+		}
+	}
+	// check all key vals are set to zero
+	for k, v := range aMap {
+		if v != 0 {
+			return false
+		}
+	}
+	return true
+}
+
 func main() {
 	fmt.Println("vim-go")
 }
